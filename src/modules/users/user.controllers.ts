da123f20e68bf209e4dda97d581fa55aeba49026
RiegL <leoorieg@gmail.com>
2024-10-31 22:27:47 -0300

    crud completo user

--- src/modules/users/user.controllers.ts
+++ src/modules/users/user.controllers.ts
@@ -1,5 +1,6 @@
-import { Controller,Get } from '@nestjs/common';
+import { Controller,Get, Post, Body, HttpCode, Param, Patch, Delete, Res} from '@nestjs/common';
 import { UserService } from './users.services';
+import { User } from '@prisma/client';
 
 @Controller('users')
 export class UserController {
@@ -7,7 +8,31 @@ export class UserController {
 
 
     @Get()
-    getUsers(){
-        return this.userService.getUsers();
+    async list(){
+       return  await this.userService.list();
+    }
+
+    @Get(':id')
+    async getById(@Param('id') id:string){
+        return  await this.userService.getById(id);
+    }
+
+
+    @Post()
+    @HttpCode(201)
+    async createUsers(@Body() body:any){
+      return await this.userService.createUsers(body);
+    }
+
+    @Patch(':id')
+    @HttpCode(200)
+    async update(@Param('id') id:string, @Body() body:any){
+        return await this.userService.update(id, body);
+    }
+    
+    @Delete(':id')
+    @HttpCode(204)
+    async remove(@Param('id') id:string){
+        return await this.userService.remove(id);
     }
 }
--- src/modules/users/users.services.ts
+++ src/modules/users/users.services.ts
@@ -1,9 +1,27 @@
-import { Injectable } from "@nestjs/common";
-
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
-export class UserService{
-    getUsers() {
-       return 'All users'
-    }
+export class UserService {
+  constructor(private readonly prisma: PrismaService){}
+  
+  createUsers(body: any):Promise<User> {
+    return this.prisma.user.create({ data: body });
+    
+  }
+  list() {
+    return this.prisma.user.findMany();
+  }
+  
+  getById(id:string) {
+    return this.prisma.user.findUnique({ where: { id:Number(id) } });
+  }
+  update(id: string, body: any) {
+    return this.prisma.user.update({ where: { id:Number(id) }, data: body })
+  }
+
+  remove(id: string) {
+    return this.prisma.user.delete({ where: { id: Number(id) } });
+  }
 }
