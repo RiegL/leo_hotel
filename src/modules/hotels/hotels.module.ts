340eabed131d1b4437c8900e04b561ffab336d1f
RiegL <leoorieg@gmail.com>
2025-01-23 21:53:15 -0300

    feat: add multer module - hotels

--- src/modules/hotels/hotels.module.ts
+++ src/modules/hotels/hotels.module.ts
@@ -12,9 +12,25 @@ import { FindByNameHotelsService } from './services/findByNameHotel.service';
 import { FindByOwnerHotelsService } from './services/findByOwnerHotel.service';
 import { AuthModule } from '../auth/auth.module';
 import { UserModule } from '../users/user.module';
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './uploads',
+        filename: (req, file, cb) => {
+          const filename = `${uuidv4()}-${file.originalname}`;
+          return cb(null, filename);
+        },
+      }),
+    }),
+  ],
   controllers: [HotelsController],
   providers: [
     CreateHotelsService,
@@ -25,10 +41,10 @@ import { UserModule } from '../users/user.module';
     RemoveHotelsService,
     FindByNameHotelsService,
     FindByOwnerHotelsService,
-   {
-    provide:HOTEL_REPOSITORY_TOKENS,
-    useClass:HotelsRepositories,
-   }
+    {
+      provide: HOTEL_REPOSITORY_TOKENS,
+      useClass: HotelsRepositories,
+    },
   ],
 })
 export class HotelsModule {}
